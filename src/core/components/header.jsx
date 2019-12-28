0cd946803eb0fc29fd83fb6943e79bdd4a642107
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-28 16:48:35 +0700

    ;sparkles: Styled header

--- src/core/components/header.jsx
+++ src/core/components/header.jsx
@@ -6,26 +6,30 @@ import { Flex, Image, Box, Button } from '@chakra-ui/core'
 
 const HeaderComponent = props => {
   return (
+    <Box bg='#5A6C73'>
     <Flex
       align='center'
       justifyContent='space-between'
-      px={8}
       py={4}
       bg='#5A6C73'
-      zIndex={1}>
+      zIndex={1}
+      width='100%'
+      maxWidth={1200}
+      mx='auto'>
       <Image size='sm' src='/logo.png' height='40px' width='auto' />
       <Flex align='center' justifyContent='center'>
         <Box px={4} color='white'>
           <Link to='/'>Home</Link>
         </Box>
         <Box px={4} color='white'>
-          <Link to='/scholars'>
-            Scholars
-          </Link>
+          <Link to='/scholars'>Scholars</Link>
         </Box>
-        <Button bg='#F98770' color='white' ml={2}>Login</Button>
+        <Button bg='#F98770' color='white' ml={2}>
+          Login
+        </Button>
       </Flex>
     </Flex>
+    </Box>
   )
 }
 
